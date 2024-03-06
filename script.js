cb822b61936e1a16310ca7becd3f0c11f404d417
ornops <joshuaornopia@gmail.com>
2024-03-06 17:22:58 +0800

    Added movement to the Shooter

--- script.js
+++ script.js
@@ -7,18 +7,17 @@ const aliensRemoved = [];
 
 for(let i = 0; i < width*width;i++){
     const square = document.createElement("div");
+    square.id = i
     grid.appendChild(square)
 }
 const squares = Array.from(document.querySelectorAll(".grid div"));
 
-
-
 const alienInvaders = [
     0,1,2,3,4,5,6,7,8,9,
     15,16,17,18,19,20,21,22,23,24,
-    30,31,32,333,34,35,36,37,38,39
+    30,31,32,33,34,35,36,37,38,39
 ];
-squares[200].classList.add('shooter')
+// squares[currentShooterIndex].classList.add('shooter')
 function draw(){
 
     for (let i = 0; i < alienInvaders.length; i++) {
@@ -28,3 +27,19 @@ function draw(){
     }
 }
 draw();
+squares[currentShooterIndex].classList.add('shooter')
+
+function moveShooter(e){
+    squares[currentShooterIndex].classList.remove('shooter')
+    switch(e.key){
+        case 'ArrowLeft':
+            if(currentShooterIndex % width !==0) currentShooterIndex -=1;
+            break;
+        case 'ArrowRight':
+            if(currentShooterIndex % width < width - 1) currentShooterIndex +=1
+            break
+    }
+    squares[currentShooterIndex].classList.add('shooter')
+}
+
+document.addEventListener('keydown', moveShooter)
